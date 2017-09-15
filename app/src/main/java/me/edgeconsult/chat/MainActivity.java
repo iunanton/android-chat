7210b697faee10d2882f06d5d8c4ff70a4a74b84
Anton Iun <iunanton@naver.com>
2017-09-16 02:23:58 +0800

    Create WebSocket

---
+++ app/src/main/java/me/edgeconsult/chat/MainActivity.java
@@ -0,0 +1,41 @@
+package me.edgeconsult.chat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Chat";
+
+    private OkHttpClient client;
+
+    private final class EchoWebSocketListener extends WebSocketListener {
+        private static final int NORMAL_CLOSURE_STATUS = 1000;
+
+        @Override
+        public void onOpen(WebSocket webSocket, Response response) {
+            Log.i(TAG, "onOpen");
+            }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        client = new OkHttpClient();
+
+        Request request = new Request.Builder().url("ws://172.17.0.5").build();
+        EchoWebSocketListener listener = new EchoWebSocketListener();
+        WebSocket ws = client.newWebSocket(request, listener);
+
+        client.dispatcher().executorService().shutdown();
+    }
+}
