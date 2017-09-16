0d4ad459843d2f17855b058060a6f6e4b4eff890
Anton Iun <iunanton@naver.com>
2017-09-16 08:34:02 +0800

    Establish WebSocket connetion

--- app/src/main/java/me/edgeconsult/chat/MainActivity.java
+++ app/src/main/java/me/edgeconsult/chat/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -19,9 +22,19 @@ public class MainActivity extends AppCompatActivity {
     private final class EchoWebSocketListener extends WebSocketListener {
         private static final int NORMAL_CLOSURE_STATUS = 1000;
 
+        private final static String jString =
+                "{"
+                        + " \"type\": \"login\","
+                        + " \"data\": {"
+                        + "             \"username\": \"ANDROID\","
+                        + "             \"password\": \"logcat\""
+                        + "           }"
+                        + "}";
+
         @Override
         public void onOpen(WebSocket webSocket, Response response) {
             Log.i(TAG, "onOpen");
+            webSocket.send(jString);
             }
     }
 
@@ -32,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         client = new OkHttpClient();
 
-        Request request = new Request.Builder().url("ws://172.17.0.5").build();
+        Request request = new Request.Builder().url("ws://172.17.0.6").build();
         EchoWebSocketListener listener = new EchoWebSocketListener();
         WebSocket ws = client.newWebSocket(request, listener);
 
